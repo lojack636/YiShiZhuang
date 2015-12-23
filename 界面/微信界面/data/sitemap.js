dafa0bfb0952b10139d429ca9158220d370c3926
linyuxiangfly <280789588@qq.com>
2015-12-23 18:15:18 +0800

    修改
    修改

--- 界面/微信界面/data/sitemap.js
+++
@@ -1,8 +0,0 @@
-﻿var sitemap = 
-
-(function() {
-    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h,i,[_(c,j,e,f,g,k),_(c,l,e,f,g,m),_(c,n,e,f,g,o),_(c,p,e,f,g,q),_(c,r,e,f,g,s),_(c,t,e,f,g,u),_(c,v,e,f,g,w),_(c,x,e,f,g,y),_(c,z,e,f,g,A),_(c,B,e,f,g,C),_(c,D,e,f,g,E),_(c,F,e,f,g,G),_(c,H,e,f,g,I),_(c,J,e,f,g,K),_(c,L,e,f,g,M),_(c,N,e,f,g,O)])]);}; 
-var b="rootNodes",c="pageName",d="主页",e="type",f="Wireframe",g="url",h="主页.html",i="children",j="订单登记",k="订单登记.html",l="我的资料",m="我的资料.html",n="我的资料页面",o="我的资料页面.html",p="订单登记页面",q="订单登记页面.html",r="商场页面",s="商场页面.html",t="商场",u="商场.html",v="订单记录",w="订单记录.html",x="订单记录页面",y="订单记录页面.html",z="订单详细",A="订单详细.html",B="订单详细2",C="订单详细2.html",D="付款页面",E="付款页面.html",F="评价页面",G="评价页面.html",H="微信公众号页",I="微信公众号页.html",J="登录页面",K="登录页面.html",L="注册页面",M="注册页面.html",N="推荐码",O="推荐码.html";
-return _creator();
-})();
--- 界面/微信界面/订单登记页面_files/axurerp_pagespecificscript.js
+++
@@ -1,7 +0,0 @@
-﻿for(var i = 0; i < 66; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
-
-$axure.eventManager.pageLoad(
-function (e) {
-
-});
-gv_vAlignTable['u51'] = 'top';gv_vAlignTable['u25'] = 'top';gv_vAlignTable['u16'] = 'top';gv_vAlignTable['u55'] = 'top';gv_vAlignTable['u46'] = 'top';gv_vAlignTable['u48'] = 'center';gv_vAlignTable['u27'] = 'top';gv_vAlignTable['u8'] = 'top';gv_vAlignTable['u23'] = 'top';gv_vAlignTable['u53'] = 'top';gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u38'] = 'top';gv_vAlignTable['u64'] = 'top';gv_vAlignTable['u49'] = 'top';gv_vAlignTable['u45'] = 'top';gv_vAlignTable['u22'] = 'center';gv_vAlignTable['u43'] = 'center';gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u6'] = 'top';gv_vAlignTable['u20'] = 'top';gv_vAlignTable['u31'] = 'top';gv_vAlignTable['u35'] = 'top';gv_vAlignTable['u12'] = 'top';gv_vAlignTable['u33'] = 'top';gv_vAlignTable['u18'] = 'top';gv_vAlignTable['u37'] = 'center';gv_vAlignTable['u10'] = 'top';gv_vAlignTable['u40'] = 'top';gv_vAlignTable['u14'] = 'top';gv_vAlignTable['u29'] = 'top';gv_vAlignTable['u4'] = 'top';
